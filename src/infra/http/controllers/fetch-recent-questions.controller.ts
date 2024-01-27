3fd25d15a943da9dc90ab0246f619b61ac1d4018
Daniel Levi <daniellevi000@gmail.com>
2024-01-27 10:15:33 -0300

    create use case autheticat user and register-student

--- src/infra/http/controllers/fetch-recent-questions.controller.ts
+++ src/infra/http/controllers/fetch-recent-questions.controller.ts
@@ -3,6 +3,7 @@ import { JwtAuthGuard } from "@/infra/auth/jwt.auth.guard";
 import { z } from "zod";
 import { ZodValidationPipe } from "@/infra/http/pipes/zod-validation-pipe";
 import { FetchRecentQuestionsUseCase } from "@/domain/forum/application/use-cases/fetch-recent-questions";
+import { QuestionPresenter } from "../presenters/question-presenter";
 
 const pageQueryParamsSchema = z.string().optional().default('1').transform(Number).pipe(
   z.number().min(1)
@@ -24,11 +25,17 @@ export class FetchRecentQuestionsController{
 @Get()
   async handle(@Query('page', queryValidationPipe) page: PageQueryParamsSchema) {
 
-    const questions = await this.fetchRecentQuestions.execute({
+    const result = await this.fetchRecentQuestions.execute({
       page
     })
+
+    if(result.isLeft()) {
+      throw new Error()
+    }
+
+    const questions = result.value.questions
     return {
-      questions
+      questions: questions.map(QuestionPresenter.toHttp)
     }
   }
 }
